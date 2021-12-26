44d444a5175aa50529372d1a69a966c2247069eb
Cole Fortner <colefortner@gmail.com>
2021-12-25 22:16:51 -0500

    Add logic for a dynamic route to test in heroku

--- routes/businesses-routes.js
+++ routes/businesses-routes.js
@@ -2,9 +2,25 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  console.log("Get Request");
-  res.json({ message: "it works" });
+const business_data = [
+  {
+    id: "1",
+    name: "Pinellas Ale House",
+    description: "bar"
+  },
+  {
+    id: "2",
+    name: "Three Daughters Brewing",
+    description: "bar"
+  }
+];
+
+router.get("/:id", (req, res, next) => {
+  const businessId = req.params.id;
+  const business = business_data.find((b) => {
+    return b.id === businessId;
+  });
+  res.json({ business: business });
 });
 
 module.exports = router;

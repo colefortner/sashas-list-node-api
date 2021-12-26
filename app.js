76eeb17b79c08afab62bb0e24ea1ef1a757d2c6a
Cole Fortner <colefortner@gmail.com>
2021-12-25 21:46:17 -0500

    Add and wire up basic business route for testing on heroku

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
+const businessRoutes = require("./routes/businesses-routes");
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("hello");
-});
+app.use("/api/businesses", businessRoutes);
 
 app.listen(process.env.PORT || 5555, () => {
   console.log("Serving on port 5555");

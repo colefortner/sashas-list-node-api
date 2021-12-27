62643abaa0f5061b211df7d1a33cf81b8a949241
Cole Fortner <colefortner@gmail.com>
2021-12-26 22:56:15 -0500

    Add middleware to attach headers to defeat Cors

--- app.js
+++ app.js
@@ -5,6 +5,19 @@ const businessRoutes = require("./routes/businesses-routes");
 
 const app = express();
 
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+
+  next();
+});
+
 app.use("/api/businesses", businessRoutes);
 
 app.listen(process.env.PORT || 5555, () => {
